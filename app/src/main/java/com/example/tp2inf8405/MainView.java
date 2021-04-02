8cc062d665ad1a4fb84a8260d6636aa4c14c5e83
Yassine Belkacemi <37895456+CakeNoFake@users.noreply.github.com>
2021-04-02 17:53:42 -0400

    dark theme wip

--- app/src/main/java/com/example/tp2inf8405/MainView.java
+++ app/src/main/java/com/example/tp2inf8405/MainView.java
@@ -1,6 +1,7 @@
 package com.example.tp2inf8405;
 
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,18 +9,47 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainView extends AppCompatActivity {
+
+    boolean isDarkMode = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test);
+
+
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        Button btn = findViewById(R.id.btn);
+        ConstraintLayout container = (ConstraintLayout) findViewById(R.id.mainView);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isDarkMode) {
+                    container.setBackgroundResource(R.color.black);
+                    btn.setBackgroundColor(Color.rgb(77, 0, 153));
+                    btn.setTextColor(Color.WHITE);
+                    isDarkMode = true;
+                }
+                 else if(isDarkMode) {
+                    container.setBackgroundResource(R.color.white);
+                    btn.setBackgroundColor(Color.rgb(204, 153, 255));
+                    btn.setTextColor(Color.BLACK);
+                    isDarkMode = false;
+                }
+            }
+        });
+
+
     }
 }
